ff1ee11400527923a393c36f59f347e7bac0aa8a
Schäffer Krisztián <schaffer.krisztian@gmail.com>
2020-10-08 12:14:26 +0200

    Documentation

--- src/Plugins.jl
+++ src/Plugins.jl
@@ -40,6 +40,27 @@ Plugins.jl does not (yet) helps with this, application developers should do it m
 """
 shutdown!(plugin::Plugin, sharedstate) = nothing
 
+"""
+    customfield(plugin::Plugin, abstract_type::Type)
+
+Provide field specifications to plugin-assembled types.
+
+Using this lifecycle hook the system can define custom plugin-assembled types
+(typically structs) based on field specifications provided by plugins.
+E.g. an error type can be extended with debug information.
+
+A plugin can provide zero or one field to every assembled type.
+To provide a field, return a [`FieldSpec`](@ref).
+    
+The assembled type will be a subtype of `abstract_type`. To allow differently
+configured systems to run in the same Julia session, new types may be assembled
+for every instance of the system.
+
+!!! warning "Metaprogramming may make you unhappy"
+    Although plugin-assmebled types are designed to help doing metaprogramming in a controlled
+    fashion, it is usually better to use non-meta solutions instead. E.g. Store plugin state
+    inside the plugin, collect data from multiple plugins using lifecycle hooks, etc.
+"""
 customfield(plugin::Plugin, abstract_type::Type) = nothing
 
 """
@@ -184,7 +205,6 @@ Returns a NamedTuple with an entry for every handler.
 cache = hook_cache([hook1, hook2], app)
 cache.hook1()
 ```
-
 """
 function hook_cache(handlers, sharedstate)
     return (;(nameof(hook) => hooklist(sharedstate, hook) for hook in handlers)...)
@@ -215,9 +235,45 @@ shutdown!(stack::PluginStack, sharedstate) = shutdown_hook!(stack, sharedstate)
 customfield(stack::PluginStack, abstract_type::Type) = customfield_hook(stack, abstract_type)
 
 abstract type TemplateStyle end
+
+"""
+    struct ImmutableStruct <: TemplateStyle end
+
+Plugin-assembled types marked as `ImmutableStruct` will be generated as a `struct`.
+"""
 struct ImmutableStruct <: TemplateStyle end
+
+"""
+    struct MutableStruct <: TemplateStyle end
+
+Plugin-assembled types marked as `MutableStruct` will be generated as a `mutable struct`.
+"""
 struct MutableStruct <: TemplateStyle end
 
+"""
+    TemplateStyle(::Type) = MutableStruct()
+
+Trait to select the template used for plugin-assembled types
+
+Use [`MutableStruct`](@ref) (default), [`ImmutableStruct`](@ref), or subtype
+it when you want to create your own template.
+
+#Examples
+
+Assembling immutable structs:
+
+```julia
+abstract type DebugInfo end
+Plugins.TemplateStyle(::Type{DebugInfo}) = Plugins.ImmutableStruct()
+```
+
+Defining your own template (see also  [`typedef`](@ref) ):
+
+```julia
+struct CustomTemplate <: Plugins.TemplateStyle end
+Plugins.TemplateStyle(::Type{State}) = CustomTemplate()
+```
+"""
 TemplateStyle(::Type) = MutableStruct()
 
 struct FieldSpec
@@ -225,6 +281,14 @@ struct FieldSpec
     type::Type
     constructor::Union{Function, DataType}
 end
+"""
+    FieldSpec(name, type::Type, constructor::Union{Function, DataType} = type)
+
+Field specification for plugin-assembled types.
+
+Note that every field of an assembled type will be constructed with the same arguments.
+ Possibly The constructor will be called when the system 
+"""
 FieldSpec(name, type::Type, constructor::Union{Function, DataType} = type) = FieldSpec(Symbol(name), type, constructor)
 
 structfield(spec::FieldSpec) = :($(spec.name)::$(Meta.parse(string(spec.type))))
@@ -246,6 +310,14 @@ function default_constructor(spec)
     return retval
 end
 
+"""
+    typedef(templatestyle, spec::TypeSpec)::Expr
+
+Return an expression defining a type.
+
+Implement it for your own template styles. More info in the
+[tests](https://github.com/tisztamo/Plugins.jl/blob/master/test/customfields.jl).
+"""
 function typedef end
 
 typedef(::MutableStruct, spec::TypeSpec) = quote
@@ -262,14 +334,43 @@ typedef(::ImmutableStruct, spec::TypeSpec) = begin
     return mutabledef
 end
 
-function customtype(stack::PluginStack, typename::Symbol, parent_type::Type = Any, target_module::Module = Main)
-    hookres = customfield(stack, parent_type)
+"""
+    customtype(stack::PluginStack, typename::Symbol, abstract_type::Type, target_module::Module = Main)
+
+Assemble a type with fields provided the plugins in `stack`.
+
+`abstract_type` will be the supertype of the assembled type.
+
+# Examples
+
+Assembling a type `AppStateImpl <: AppState` and parametrizing the app with
+it. 
+```julia
+mutable struct CustomFieldsApp{TCustomState}
+    state::TCustomState
+    function CustomFieldsApp(plugins, hookfns, stateargs...)
+        stack = PluginStack(plugins, hookfns)
+        state_type = customtype(stack, :AppStateImpl, AppState)
+        return new{state_type}(Base.invokelatest(state_type, stateargs...))
+    end
+end
+```
+!!! note "The need for `invokelatest`"
+    We need to use `invokelatest` to instantiate a newly generated type. To use 
+    the generated type normally, first you have to allow control flow to go to the top-level
+    scope after the type was generated. See also the [docs](https://docs.julialang.org/en/v1/manual/methods/#Redefining-Methods)
+!!! warning "Antipattern"
+    Assembling state types is an antipattern, because plugins can have their own state.
+    (This may provide better performance in a few cases though)
+"""
+function customtype(stack::PluginStack, typename::Symbol, abstract_type::Type, target_module::Module = Main)
+    hookres = customfield(stack, abstract_type)
     if !hookres.allok
         throw(ErrorException("Cannot define custom type, a plugin throwed an error: $hookres"))
     end
     fields = hookres.results
-    spec = TypeSpec(typename, target_module, parent_type, fields)
-    def = typedef(TemplateStyle(parent_type), spec)
+    spec = TypeSpec(typename, target_module, abstract_type, fields)
+    def = typedef(TemplateStyle(abstract_type), spec)
     return Base.eval(target_module, def)
 end
 
